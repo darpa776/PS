a6ecde5666a1cd5a2be02347840c0268553b3673
repl.it user <replituser@example.com>
2020-09-23 08:13:48 +0000

    0923 update

--- io.cpp
+++ io.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 int main()
 {
-  // //2557: Hello world
+  //2557: Hello world
   // cout<< "Hello World!" <<endl;
 
-  // //1000: A+B
+  //1000: A+B
   // //2558: A+B-2
   // int A,B;
   // cin>> A>>B;
   // cout<<A+B;
 
-  // //10950: A+B-3
+  //10950: A+B-3
   // int T;
   // int test_num=0;
   // cin>>T;
@@ -27,16 +27,42 @@ int main()
   //   test_num++;
   // }
 
-  //  //10951: A+B-4
-  //  //(testcase가 주어지지 않는 경우)
+  //10951: A+B-4
+  //(testcase가 주어지지 않는 경우)
   // int A,B;
   // while(cin>> A>>B)
   // {
   // cout<<A+B<<endl;
   // }
 
-//
+  //10952: A+B-5
+  // int A,B;
+  // while(1){
+  // cin>> A>>B;
+  // if(A==0&&B==0)
+  // {
+  //   return 0;
+  // }
+  // cout<<A+B<<endl;
+  // }
 
+//10953: A+B-6
+//  int A,B;
+//  char blank;
+//  int testcase=0;
+//  int T;
+//  cin>>T;
+//  while(testcase<T)
+//  {
+//    cin>>A>>blank>>B;
+//    cout<<A+B<<endl;
+//    testcase++;
+//  }
+// //blank를 문자형으로 받아야함.
+// //2,2 공백없이 이어져 있어서
+// //A를 2,2로 인식하기 때문에 오류남
+
+//
 
 
   return 0;
