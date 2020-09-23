ebad61f14a412800740f36425ec0d0e6c02af6ed
repl.it user <replituser@example.com>
2020-09-23 05:31:22 +0000

    0923update

--- io.cpp
+++ io.cpp
@@ -2,10 +2,44 @@
 
 using namespace std;
 
-//2557: Hello world
+
 int main()
 {
-cout<< "Hello World!" <<endl;
-return 0;
+  // //2557: Hello world
+  // cout<< "Hello World!" <<endl;
+
+  // //1000: A+B
+  // //2558: A+B-2
+  // int A,B;
+  // cin>> A>>B;
+  // cout<<A+B;
+
+  // //10950: A+B-3
+  // int T;
+  // int test_num=0;
+  // cin>>T;
+
+  // while(test_num<T)
+  // {
+  //   int A,B;
+  //   cin>> A>>B;
+  //   cout<<A+B<<endl;
+  //   test_num++;
+  // }
+
+  //  //10951: A+B-4
+  //  //(testcase가 주어지지 않는 경우)
+  // int A,B;
+  // while(cin>> A>>B)
+  // {
+  // cout<<A+B<<endl;
+  // }
+
+//
+
+
+
+  return 0;
 }
+
 
