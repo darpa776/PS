7c0ce17e6a68a0f864b6e5a079189613aad734a8
darpa776 <nana970@naver.com>
2021-01-20 10:18:15 +0900

    Update 7럭키스트레이트.cpp

--- dongbinna_CT_실전/7럭키스트레이트.cpp
+++ dongbinna_CT_실전/7럭키스트레이트.cpp
@@ -7,21 +7,44 @@ using namespace std;
 
 int main()
 {
-	string s;
-	cin >> s;
-	int num = 0;
-	vector<char> newS;
-	for(int i=0;i<s.size();i++)
-	{//아래 if문 또는 isalpha 함수를 사용하면 알파벳인지 알 수 있다.
-		if (s[i] < 65)//숫자
-			num += s[i]-'0';
-		else//알파벳대문자
-			newS.push_back(s[i]);
-	}
-	sort(newS.begin(), newS.end());
-	for (int i = 0; i < newS.size(); i++)
+	int n;
+	cin >> n;
+
+	int origin = n;
+
+	vector<int> number;
+	while(n>10)
 	{
-		cout << newS[i];
+		number.push_back(n % 10);
+		n /= 10;
 	}
-	cout << num << endl;
+	number.push_back(n);
+	int m = number.size() / 2;
+	//앞뒤로 더해서 같은지 확인
+	int sum1 = 0;
+	int sum2 = 0;
+	for(int i=0;i<m;i++)
+	{
+		sum1 += number[i];
+		sum2 += number[2 * m - i-1];
+	}
+	if (sum1 == sum2)
+		cout << "LUCKY";
+	else
+		cout << "READY";
 }
+
+//// 왼쪽 부분의 자릿수의 합 더하기
+////정수로 들어오는 입력을 str으로 입력받아서 size()함수로 자릿수 바로 알아내기
+//for (int i = 0; i < str.size() / 2; i++) {
+//	summary += str[i] - '0';
+//}
+//
+//// 오른쪽 부분의 자릿수의 합 빼기
+//for (int i = str.size() / 2; i < str.size(); i++) {
+//	summary -= str[i] - '0';
+//}
+//
+//// 왼쪽 부분과 오른쪽 부분의 자릿수 합이 동일한지 검사
+//if (summary == 0) cout << "LUCKY" << '\n';
+//else cout << "READY" << '\n';
