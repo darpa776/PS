f093a59e877c79a8f08b6316b97709b17da29f8c
darpa776 <nana970@naver.com>
2021-01-25 16:20:50 +0900

    Update _25실패율.cpp

--- dongbinna_CT_실전/_25실패율.cpp
+++ dongbinna_CT_실전/_25실패율.cpp
@@ -3,7 +3,6 @@
 #include <algorithm>
 using namespace std;
 vector<pair<float, int>> result;
-vector<int> stages;//입력받는 벡터
 vector<int> answer;//solution 함수의 리턴형
 bool cmp(pair<float, int> a, pair<float, int> b) {
 
@@ -24,9 +23,22 @@ bool cmp(pair<float, int> a, pair<float, int> b) {
 		//cnt= i단계에 해당하는 사람들 수
 		//실패율, stage num 입력
 		//실패율= 스테이지에 도달했으나 아직 클리어하지 못한 유저 수/스테이지에 도달한 유저 수
-		result.push_back({ (float)person / length,i });
+        
+        // 실패율 계산
+        float fail = 0;
+        if (length >= 1) {
+          fail = (float) person / length;
+          }
+        
+		result.push_back({ fail,i });
+        length-=person;
 	//마지막 단계를 통과한 사람은 분모인 length에 다 포함되어 있음!!
 	}
+        
+     
+        
+        
+        
 	sort(result.begin(), result.end(), cmp);
 	//정렬다하고 나면 실패율 순서대로 stage 출력하기
 	for (int i = 0; i < result.size(); i++)
