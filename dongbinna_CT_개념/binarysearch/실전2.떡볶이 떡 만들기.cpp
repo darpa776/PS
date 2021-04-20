8a7ca521a18c32ef6bce72d918e2c41ee30750b9
darpa776 <nana970@naver.com>
2021-04-20 10:10:38 +0900

    Update 실전2.떡볶이 떡 만들기.cpp

--- dongbinna_CT_개념/binarysearch/실전2.떡볶이 떡 만들기.cpp
+++ dongbinna_CT_개념/binarysearch/실전2.떡볶이 떡 만들기.cpp
@@ -1,67 +1,53 @@
+//떡볶이 떡 확인
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
-
-vector<int> arr;
-int n, m;
-//떡의 개수 n, 요청한 떡의 길이 m
+int n, m;// 떡의 개수, 요청한 떡의 길이
+vector<int> bread;
 int ans;
 
-int cut(int key)
-{
-	int sum=0;
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] - key > 0)
-			sum += (arr[i] - key);
+void binary_search(int start, int end) {//매개변수로 받는 것은 높이임
+	if (start > end) return;//start와 end가 같을 경우도 이진탐색 함수를 돌게 설계
+	int mid = (start + end) / 2;
+	//mid에서 자른다.
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		if(bread[i] - mid>0)
+			sum += (bread[i]-mid);
 	}
-	return sum;
+		//만약 자른 길이가 부족할경우
+	if (sum < m){
+		
+		return binary_search(start, mid-1);
+		//항상 end는 mid-1
+	}
+	//만약 자른 길이가 손님이 주문할 양보다 많거나 같을 경우
+	else if (sum >= m) {
+		ans = mid;
+		return binary_search(mid+1, end);
+		//항상 start는 mid+1
+	}
+	
+
+	return;
 }
 
-int main()
-{
-	
+int main() {
+	//절단기 높이: H, 잘린 떡의 길이만큼을 손님이 가져가신다.
+	//손님이 요청한 길이는 m, m이상의 떡을 줘야한다.
 	cin >> n >> m;
-	
-	for (int i = 0; i < n; i++)
-	{
-		int x;
-		cin >> x;
-		arr.push_back(x);
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		cin >> tmp;
+		bread.push_back(tmp);
 	}
+	//가장 낮은 가래떡의 길이부터, 가장 높은 길이의 가래떡 사이에서 잘라야한다.
+	//->0cm부터 가장 높은 가래떡 길이
+	sort(bread.begin(), bread.end());
+	binary_search(0, bread[n - 1]);
 
-	//이진탐색,,자를 길이를 전부 다 탐색하는 것을 편히 하기 위해 이진탐색 사용
-	//0이 start, 최대길이가 end
-	int start = 0;
-	sort(arr.begin(), arr.end());
-	int end = arr[n - 1];
-	
-
-	//이진탐색시작
-	while (start <= end)
-	{
-		int mid = (start + end) / 2;
-		if (cut(mid) > m)
-		{
-			//더 많이 줬음
-			start = mid + 1;
-			ans = mid;
-			//돌다가 while문에 안맞으면 그대로 나갈것임
-			//현재 mid값도 정답이 될 수 있으므로 ans에 저장
-		}
-		else if (cut(mid) < m)
-		{
-			//저기요 제가 주문한 양보다 더 적은데요
-			end = mid - 1;
-		}
-		else if (cut(mid) >= m)
-		{//더 많이 줬거나 딱맞는 경우
-			ans = mid;
-			break;
-		}
-		
-	}
+	//손님이 최대한 떡을 많이 가져갈수 있는 절단 높이
 	cout << ans << endl;
 	return 0;
 }
